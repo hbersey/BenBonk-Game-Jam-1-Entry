95c09c0cea9271c73e253512eb6be9aa8d0715c6
Henry Bersey <henry@bersey.com>
2020-05-13 15:03:40 +0100

    Create State Machine

---
+++ Assets/Scripts/StateMachine/State.cs
@@ -0,0 +1,24 @@
+﻿namespace StateMachine
+{
+    public abstract class State
+    {
+
+        public virtual void Begin()
+        {
+        }
+
+        public virtual void Update()
+        {
+            
+        }
+
+        public virtual void PhysicsUpdate()
+        {
+        }
+
+        public virtual void End()
+        {
+        }
+
+    }
+}
---
+++ Assets/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace StateMachine
+{
+    public abstract class StateMachine : MonoBehaviour
+    {
+        private State _state;
+
+        private void Update()
+        {
+            _state.Update();
+        }
+
+        private void FixedUpdate()
+        {
+            _state.PhysicsUpdate();
+        }
+
+        public void SetState(State state)
+        {
+            _state?.End();
+            _state = state;
+            _state.Begin();
+        }
+    }
+}
