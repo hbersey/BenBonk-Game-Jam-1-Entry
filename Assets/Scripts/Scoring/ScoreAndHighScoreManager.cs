1c9498dc9659abce0a5c970a3d8a0ac506c35d4d
Henry Bersey <henry@bersey.com>
2020-05-28 16:36:37 +0100

    Added Scoring

---
+++ Assets/Scripts/Scoring/ScoreAndHighScoreManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Scoring
+{
+    public class ScoreAndHighScoreManager : ScoreOnlyManager
+    {
+        private float _highScore;
+
+        public float HighScore
+        {
+            get
+            {
+                UpdateHighScore();
+                return _highScore;
+            }
+            set => _highScore = value;
+        }
+
+        private float _initialHighScore;
+
+        public ScoreAndHighScoreManager(float intialScore = 0f, float incrementAmount = 1f, float initialHighScore = 0f)
+            : base(intialScore, incrementAmount)
+        {
+            _initialHighScore = initialHighScore;
+            HighScore = initialHighScore;
+        }
+
+        private void UpdateHighScore()
+        {
+            if (Score > _highScore) _highScore = Score;
+        }
+    }
+}
---
+++ Assets/Scripts/Scoring/ScoreOnlyManager.cs
@@ -0,0 +1,28 @@
+﻿namespace Scoring
+{
+    public class ScoreOnlyManager
+    {
+        public float Score;
+        private readonly float _initialScore;
+        private readonly float _incrementAmount;
+
+        public ScoreOnlyManager(float initialScore = 0f, float incrementAmount = 1f)
+        {
+            Score = initialScore;
+            _initialScore = initialScore;
+            _incrementAmount = incrementAmount;
+        }
+
+        public float Increment()
+        {
+            Score += _incrementAmount;
+            return Score;
+        }
+
+        public float Reset()
+        {
+            Score = _initialScore;
+            return Score;
+        }
+    }
+}
