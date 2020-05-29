06371fda7fb7479acd7b2ba4bbfc209ff241c60a
Henry Bersey <henry@bersey.com>
2020-05-29 21:44:15 +0100

    Removed redundant examples and added player movement

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Player
+{
+    [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+    public class PlayerController: StateMachine.StateMachine<PlayerState>
+    {
+        [SerializeField] protected internal float speed = 10f;
+    
+        protected internal Rigidbody2D Rigidbody; 
+        
+        private void Start()
+        {
+            Rigidbody = GetComponent<Rigidbody2D>();
+
+            SetState(new PlayerMoveState(this));   
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerState.cs
@@ -0,0 +1,12 @@
+﻿namespace Player
+{
+    public abstract class PlayerState : StateMachine.State
+    {
+        protected readonly PlayerController Player;
+
+        protected PlayerState(PlayerController player)
+        {
+            Player = player;
+        }
+    }
+}
