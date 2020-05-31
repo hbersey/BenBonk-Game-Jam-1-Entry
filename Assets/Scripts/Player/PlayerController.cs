0be799b3ee66cf6d8d127b7066cf795dfbc95b94
Henry Bersey <henry@bersey.com>
2020-05-31 11:50:46 +0100

    Player Animation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,17 +4,18 @@ using UnityEngine.Serialization;
 
 namespace Player
 {
-    [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+    [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D), typeof(Animator))]
     public class PlayerController: StateMachine.StateMachine<PlayerState>
     {
-        [SerializeField] protected internal float speed = 10f;
+        [SerializeField] protected internal float movementSpeed = 10f;
     
-        protected internal Rigidbody2D Rigidbody; 
-        
+        protected internal Rigidbody2D Rigidbody;
+        protected internal Animator Animator;
         private void Start()
         {
             Rigidbody = GetComponent<Rigidbody2D>();
-
+            Animator = GetComponent<Animator>();
+            
             SetState(new PlayerMoveState(this));   
         }
     }
--- Assets/Scripts/Player/PlayerMoveState.cs
+++ Assets/Scripts/Player/PlayerMoveState.cs
@@ -1,19 +1,34 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Player
 {
     public class PlayerMoveState : PlayerState
     {
+        private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+        private static readonly int Vertical = Animator.StringToHash("Vertical");
+        private static readonly int Moving = Animator.StringToHash("Moving");
+        
+        private float _horizontal;
+        private float _vertical;
+
         public PlayerMoveState(PlayerController player) : base(player)
         {
         }
 
+        public override void Update()
+        {
+            _horizontal = Input.GetAxisRaw("Horizontal");
+            _vertical = Input.GetAxisRaw("Vertical");
+
+            Player.Animator.SetFloat(Horizontal, _horizontal);
+            Player.Animator.SetFloat(Vertical, _vertical);
+        }
+
         public override void PhysicsUpdate()
         {
-            var horizontalMovement = Input.GetAxisRaw("Horizontal") * Player.speed;
-            var verticalMovement = Input.GetAxisRaw("Vertical") * Player.speed;
-            
-            Player.Rigidbody.velocity = new Vector2(horizontalMovement, verticalMovement);
+            Player.Rigidbody.velocity = new Vector2(_horizontal, _vertical) * Player.movementSpeed;
+            Player.Animator.SetBool(Moving, Math.Abs(_horizontal) > 0.1f || Math.Abs(_vertical) > 0.1f);
         }
     }
 }
