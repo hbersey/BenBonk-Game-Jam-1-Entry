be7dd612195b6ac373bff910ccb35af455d1f686
Henry Bersey <henry@bersey.com>
2020-06-01 12:57:59 +0100

    Item Sounds

--- Assets/Scripts/Item/SanitizerController.cs
+++ Assets/Scripts/Item/SanitizerController.cs
@@ -1,13 +1,19 @@
-﻿using Game;
+﻿using System;
+using System.Collections;
+using Game;
 using Player;
 using UnityEngine;
 
 namespace Item
 {
-    [RequireComponent(typeof(CapsuleCollider2D))]
+    [RequireComponent(typeof(CapsuleCollider2D), typeof(AudioSource))]
     public class SanitizerController : MonoBehaviour
     {
         internal GameManager Game;
+
+        private AudioSource _audioSource;
+        private void Start() => _audioSource = GetComponent<AudioSource>();
+         
 
         private void OnTriggerEnter2D(Collider2D other)
         {
@@ -15,10 +21,18 @@ namespace Item
             if (!(Game.State is RoundState state)) return;
             if (!(Game.player.State is PlayerMoveState)) return;
 
+            StartCoroutine(Collect(state));
+        }
+
+        private IEnumerator Collect(RoundState state)
+        {
             if (Game.Health >= 3)
                 Game.AddScore(state.PointsPerItem * 5);
             else Game.PickupHealth();
 
+            _audioSource.Play();
+            yield return new WaitForSeconds(_audioSource.clip.length);
+            
             Destroy(gameObject);
         }
     }
