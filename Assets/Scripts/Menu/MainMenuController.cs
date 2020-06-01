219a9831fe37f39f034824f743ba74e85386720d
Henry Bersey <henry@bersey.com>
2020-06-01 09:45:44 +0100

    Credits Page

--- Assets/Scripts/Menu/MainMenuController.cs
+++ Assets/Scripts/Menu/MainMenuController.cs
@@ -1,22 +1,38 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Menu
 {
     public class MainMenuController : MonoBehaviour
     {
+        [SerializeField] private GameObject menu;
+        [SerializeField] private GameObject credits;
+
+        private void Start()
+        {
+            ShowMenu();
+        }
+
         public void Play()
         {
             SceneManager.LoadScene("Scenes/Game", LoadSceneMode.Single);
         }
 
-        public void Credits()
+        public void ShowCredits()
         {
+            menu.SetActive(false);
+            credits.SetActive(true);
         }
-
         public void Exit()
         {
             Application.Quit();
+        }
+
+        public void ShowMenu()
+        {
+            credits.SetActive(false);
+            menu.SetActive(true);
         }
     }
 }
