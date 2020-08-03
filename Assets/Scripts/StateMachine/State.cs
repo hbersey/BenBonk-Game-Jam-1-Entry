455db68770ea205f53689f8556179f4de496ba55
Henry Bersey <henry@bersey.com>
2020-08-03 22:39:05 +0100

    State Machine and State (1.0.2)

---
+++ Assets/Scripts/StateMachine/State.cs
@@ -0,0 +1,28 @@
+﻿namespace StateMachine
+{
+    public abstract class State<T>
+    {
+        private T _machine;
+
+        protected State(T machine)
+        {
+            _machine = machine;
+        }
+
+        public virtual void OnBegin()
+        {
+        }
+
+        public virtual void OnEnd()
+        {
+        }
+
+        public virtual void OnTick()
+        {
+        }
+
+        public virtual void OnPhysicsTick()
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,22 @@
+﻿using JetBrains.Annotations;
+
+namespace StateMachine
+{
+    public class StateMachine<T>
+    {
+        [CanBeNull] private State<T> _state;
+
+        public StateMachine(State<T> initialState = null) => _state = initialState;
+
+        public void SetState(State<T> state)
+        {
+            _state?.OnEnd();
+            _state = state;
+            _state?.OnBegin();
+        }
+
+        public void Tick() => _state?.OnTick();
+
+        public void PhysicsTick() => _state?.OnPhysicsTick();
+    }
+}
