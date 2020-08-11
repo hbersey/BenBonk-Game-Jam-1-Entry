631164ae1f66a18f8580cce5b71362e8c6f117c5
Henry Bersey <henry@bersey.com>
2020-08-11 22:03:17 +0100

    Added Item Added DepartmentScriptableObject

---
+++ Assets/Scripts/World/DepartmentScriptableObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace World
+{
+    [CreateAssetMenu(fileName = "Department", menuName = "PandemicShopper/Department", order = 1)]
+    public class DepartmentScriptableObject : ScriptableObject
+    {
+        public new string name;
+        public ItemScriptableObject[] items;
+        public Sprite[] floor;
+    }
+}
---
+++ Assets/Scripts/World/ItemScriptableObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace World
+{
+    [CreateAssetMenu(fileName = "Item", menuName = "PandemicShopper/Item", order = 0)]
+    public class ItemScriptableObject : ScriptableObject
+    {
+        public new string name;
+        public Sprite sprite;
+    }
+}
